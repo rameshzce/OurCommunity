d891af2e88292343afb5f4b588ae0398ff6a2ece
rameshkolamala <rameshzce@gmail.com>
2017-04-14 04:32:00 +0530

    FBLogin

---
+++ OurCommunity/Helper.swift
@@ -0,0 +1,118 @@
+//
+//  Helper.swift
+//  OurCommunity
+//
+//  Created by Ramesh Kolamala on 13/04/17.
+//  Copyright © 2017 tokkalo.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Helper{
+    
+    
+    static func customizetextField(_ textField: UITextField!) {
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white.cgColor
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        
+        textField.leftView = paddingView
+        
+        textField.leftViewMode = UITextFieldViewMode.always
+    }
+    
+    static func customizetextField2(_ textField: UITextField!) {
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white.cgColor
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        
+        textField.leftView = paddingView
+        
+        textField.leftViewMode = UITextFieldViewMode.always
+    }
+    
+    static func customizetextField3(_ textField: UITextField!) {
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        textField.layer.cornerRadius = 3
+        textField.layer.borderWidth = 0
+        textField.layer.borderColor = UIColor.white.cgColor
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        
+        textField.leftView = paddingView
+        
+        textField.leftViewMode = UITextFieldViewMode.always
+        
+        textField.tintColor = UIColor.white
+    }
+    
+    static func customButton(_ button: UIButton!) {
+        //button.setBackgroundImage(nil, for: .normal)
+        button.backgroundColor = UIColor.clear
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.white.cgColor
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        //button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    }
+    
+    static func customButton2(_ button: UIButton!) {
+        button.setBackgroundImage(nil, for: .normal)
+        button.backgroundColor = UIColor.clear
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 0
+        button.layer.borderColor = UIColor.white.cgColor
+        button.setTitleColor(UIColor.yellow, for: UIControlState.normal)
+    }
+    
+    static func hexStringToUIColor (_ hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString = cString.substring(from: cString.characters.index(cString.startIndex, offsetBy: 1))
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    static func loadImageFromUrl(_ url: String, view: UIImageView){
+        
+        // Create Url from string
+        let url = URL(string: url)!
+        
+        // Download task:
+        // - sharedSession = global NSURLCache, NSHTTPCookieStorage and NSURLCredentialStorage objects.
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (responseData, responseUrl, error) -> Void in
+            // if responseData is not null...
+            if let data = responseData{
+                
+                // execute in UI thread
+                DispatchQueue.main.async(execute: { () -> Void in
+                    view.image = UIImage(data: data)
+                })
+            }
+        })
+        
+        // Run task
+        task.resume()
+    }
+}
--- OurCommunity/ViewController.swift
+++ OurCommunity/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if (FBSDKAccessToken.current() != nil)
+        {
+            
+            print("User Logged In")
+            
+        }
+        else
+        {
+            let loginButton : FBSDKLoginButton = FBSDKLoginButton()
+            
+            loginButton.center = self.view.center
+            
+            loginButton.readPermissions = ["public_profile", "email"]
+            
+            loginButton.delegate = self
+            
+            self.view.addSubview(loginButton)
+        }
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil
+        {
+            
+            print(error)
+            
+        }
+        else if result.isCancelled {
+            
+            print("User cancelled login")
+            
+        }
+        else {
+            
+            
+            if result.grantedPermissions.contains("email")
+            {
+                
+                if let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"]) {
+                    
+                    graphRequest.start(completionHandler: { (connection, result, error) in
+                        
+                        if error != nil {
+                            
+                            print(error)
+                            
+                        } else {
+                            
+                            if let userDetails = result as? [String: String] {
+                                
+                                print(userDetails["email"])
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    })
+                    
+                    
+                }
+                
+                
+                
+            }
+        }
     }
-
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+        print("Logged out")
+        
+    }
+    
+    
 }
+
+
 
