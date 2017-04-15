7e418cdaa4e25d8c32b4958e4cf1ec3737c79950
rameshkolamala <rameshzce@gmail.com>
2017-04-15 14:47:51 +0530

    signin changes

--- OurCommunity/AppDelegate.swift
+++ OurCommunity/AppDelegate.swift
@@ -60,13 +60,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
             // [END_EXCLUDE]
         } else {
             // Perform any operations on signed in user here.
-            let userId = user.userID                  // For client-side use only!
+            _ = user.userID                  // For client-side use only!
             
-            let idToken = user.authentication.idToken // Safe to send to the server
+            _ = user.authentication.idToken // Safe to send to the server
             let fullName = user.profile.name
-            let givenName = user.profile.givenName
-            let familyName = user.profile.familyName
-            let email = user.profile.email
+            _ = user.profile.givenName
+            _ = user.profile.familyName
+            _ = user.profile.email
             // [START_EXCLUDE]
             print("Userid: \(String(describing: fullName!))")
             NotificationCenter.default.post(
