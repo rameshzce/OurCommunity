867f3200e7c96b5f3321d466afda5ec9c3d20687
rameshkolamala <rameshzce@gmail.com>
2017-04-16 15:21:09 +0530

    fb changes

--- OurCommunity/AppDelegate.swift
+++ OurCommunity/AppDelegate.swift
@@ -16,7 +16,6 @@ import GoogleSignIn
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     var window: UIWindow?
-    var vc = ViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
